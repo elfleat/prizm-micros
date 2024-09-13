96d4c08c204ea525ba53f024558954457c69df63
Elias Sierra <elfleat@gmail.com>
2024-09-13 06:15:07 -0400

    initial commit

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the header
  *
@@ -7,37 +8,53 @@
  * @package HelloElementor
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-$viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' );
-$enable_skip_link = apply_filters( 'hello_elementor_enable_skip_link', true );
-$skip_link_url = apply_filters( 'hello_elementor_skip_link_url', '#content' );
+$viewport_content = apply_filters('hello_elementor_viewport_content', 'width=device-width, initial-scale=1');
+$enable_skip_link = apply_filters('hello_elementor_enable_skip_link', true);
+$skip_link_url = apply_filters('hello_elementor_skip_link_url', '#content');
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="<?php echo esc_attr($viewport_content); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+	<script src="https://cdn.tailwindcss.com"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+	<style type="text/css">
+		.font-dm-sans,
+		body {
+			font-family: "DM Sans", sans-serif;
+			font-optical-sizing: auto;
+			font-weight: 300;
+			font-style: normal;
+		}
+	</style>
+	<script src="https://unpkg.com/@phosphor-icons/web"></script>
 </head>
+
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<?php if ( $enable_skip_link ) { ?>
-<a class="skip-link screen-reader-text" href="<?php echo esc_url( $skip_link_url ); ?>"><?php echo esc_html__( 'Skip to content', 'hello-elementor' ); ?></a>
-<?php } ?>
+	<?php if ($enable_skip_link) { ?>
+		<a class="skip-link screen-reader-text" href="<?php echo esc_url($skip_link_url); ?>"><?php echo esc_html__('Skip to content', 'hello-elementor'); ?></a>
+	<?php } ?>
 
-<?php
-if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
-	if ( hello_elementor_display_header_footer() ) {
-		if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
-			get_template_part( 'template-parts/dynamic-header' );
-		} else {
-			get_template_part( 'template-parts/header' );
+	<?php
+	if (! function_exists('elementor_theme_do_location') || ! elementor_theme_do_location('header')) {
+		if (hello_elementor_display_header_footer()) {
+			if (did_action('elementor/loaded') && hello_header_footer_experiment_active()) {
+				get_template_part('template-parts/dynamic-header');
+			} else {
+				get_template_part('template-parts/header');
+			}
 		}
 	}
-}
